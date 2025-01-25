cab24bcd571dbfc17528da84a4796a2453fda436
Marcin Szablak <m.szablak30@gmail.com>
2025-01-25 22:40:09 +0100

    Added to disc onClick function to start whole album

--- app/src/main/java/com/example/musicapp/ui/lists/songsList/SongsList.kt
+++ app/src/main/java/com/example/musicapp/ui/lists/songsList/SongsList.kt
@@ -1,14 +1,21 @@
 package com.example.musicapp.ui.lists.songsList
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -22,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
+import com.example.musicapp.logic.mediaPlayer.AppExoPlayer
 import com.example.musicapp.logic.song.Song
 import com.example.musicapp.logic.song.getSongs
 import com.example.musicapp.logic.song.getSongsFromDatabaseWithUri
@@ -88,14 +96,35 @@ fun SongsList(
             .verticalScroll(rememberScrollState())
             .padding(10.dp)
     ) {
-        SpinningDisc(
-            albumsList = albumsList,
-            listOfUri = listUri,
-            song = firstSongFromList.value,
+        Button(
+            shape = RoundedCornerShape(0.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.background
+            ),
+            elevation = null,
+            contentPadding = PaddingValues(0.dp),
             modifier = Modifier
-                .offset( x= 0.dp, y= (-100).dp)
-                .height(180.dp)
-        )
+                .padding(0.dp)
+                .fillMaxWidth()
+                .offset( x= 0.dp, y= (-16).dp),
+            onClick = {
+                AppExoPlayer.setPlaylist(
+                    songPlaylist = discList.flatten(),
+                    albumsList = albumsList,
+                )
+                AppExoPlayer.playMusic()
+                AppExoPlayer.setPlaylistToSelectedSong(discList.flatten()[0], discList.flatten())
+            }
+        ){
+            SpinningDisc(
+                albumsList = albumsList,
+                listOfUri = listUri,
+                song = firstSongFromList.value,
+                modifier = Modifier
+                    .offset( x= 0.dp, y= (-90).dp)
+                    .height(180.dp)
+            )
+        }
         for (disc in filteredDisc){
             DiscInfoHeader(
                 songsList = disc,
