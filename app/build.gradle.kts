bd6e734e204929f87013970481ee29a81de10fa4
Marcin Szablak <m.szablak30@gmail.com>
2025-09-30 16:41:06 +0200

    Started making ArtistFragment.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/musicapp/fragments/artist/ArtistFragment.kt
+++ app/src/main/java/com/example/musicapp/fragments/artist/ArtistFragment.kt
@@ -1,4 +1,45 @@
 package com.example.musicapp.fragments.artist
 
-class ArtistFragment {
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import com.example.musicapp.FragmentStack
+import com.example.musicapp.R
+
+class ArtistFragment: Fragment()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_artist_view,
+            container,
+            false)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val returnButton: ImageButton = view.findViewById(R.id.fragment_artist_view_back)
+
+        returnButton.setOnClickListener {
+            FragmentStack.mainStack.pop()
+            val fragmentTransaction = parentFragmentManager.beginTransaction()
+            fragmentTransaction
+                .replace(R.id.main_frame, FragmentStack.mainStack.peek())
+                .setReorderingAllowed(true)
+                .addToBackStack("")
+                .commit()
+        }
+    }
 }
